e0460a94131f240b8dd607c2e9bb75be09e58883
z0mbi1998 <z0mbi1998@outlook.com>
2026-04-08 20:26:44 -0300

    desavio016

---
+++ desafios/desafio016.py
@@ -0,0 +1,18 @@
+from rich import print
+
+class Funcionario:
+    empresa = "Curso em Video" # Atributo de classe
+
+    def __init__(self, nome, setor, cargo):
+        self.nome = nome
+        self.setor = setor
+        self.cargo = cargo
+
+    def apresentar(self):
+        return f":handshake: Ola, sou [blue]{self.nome}[/] e sou {self.cargo} do setor de {self.setor} da empresa {self.empresa}"
+
+c1 = Funcionario("Maria", "Administracao", "Diretora")
+print(c1.apresentar())
+
+c2 = Funcionario("Pedro", "TI", "Programador")
+print(c2.apresentar())
